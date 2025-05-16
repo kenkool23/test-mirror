842f9c96368ed706c51bb128a66879bd9ba6bfdb
kendevops4 <ken4@k-kloud.net>
2025-05-16 11:58:47 -0400

    added versioning

--- terraform/module/s3.tf
+++ terraform/module/s3.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "s3" {
 
 resource "aws_s3_bucket_versioning" "versioning_s3" {
   bucket = aws_s3_bucket.s3.id
-  count  = var
+  count  = var.enabled_versioning ? 1 : 0
   versioning_configuration {
     status = "Enabled"
   }
